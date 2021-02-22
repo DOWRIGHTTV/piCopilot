bc4ab1043018e984f51d602a9ebcd39fd69c6b5c
DOWRIGHT <45921108+DOWRIGHTTV@users.noreply.github.com>
2021-02-22 00:04:56 -0700

    perf tweaks and general refactor.
    implemented slots and converted gpsd object into a class variable (class var lookup through an instance is faster also).

--- SRC/DEB_picopilot-idrop/opt/piCopilot-idrop/lib/location.py
+++ SRC/DEB_picopilot-idrop/opt/piCopilot-idrop/lib/location.py
@@ -1,21 +1,29 @@
 import gpsd
 
-class Location(object):
+
+class Location:
+    
+    # assign class variable for imported module gpsd for compatibility with previous revision. 
+    # in the future this object can potentially be referenced directly in other modules by 
+    # importing within the module itself instead of referencing through the Location class.
+    gpsObj = gpsd
+    
+    __slots__ = (
+        'trigger',
+    )
     
     def __init__(self):
         self.trigger = None
         try:
             gpsd.connect()
-            self.gpsObj = gpsd
-            self.trigger = True
         except:
             pass
-
+        else:
+            self.trigger = True
 
     def getCoord(self):
-        lCheck = None
+        lCheck = None # NOTE: is this needed?
         try:
-            x = self.gpsObj.get_current()
-            return x.position()
+            return self.gpsObj.get_current().position()
         except:
             return None
